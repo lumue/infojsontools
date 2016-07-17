b6a611fff48dcde7287b7bee69e52ea444d70326
lm <mueller.lutz@gmail.com>
2016-07-17 08:30:27 +0200

    empty test

--- src/test/java/io/github/lumue/infojson/InfojsonReaderTest.java
+++ src/test/java/io/github/lumue/infojson/InfojsonReaderTest.java
@@ -2,11 +2,6 @@ package io.github.lumue.infojson;
 
 import org.junit.Test;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import java.io.InputStream;
-
-import static org.junit.Assert.*;
 
 /**
  * test nfo serialization of a movie entity
