8f60575bc29348b2b7b615ff62f7ff0998466784
lm <mueller.lutz@gmail.com>
2016-07-17 08:18:55 +0200

    initial import

--- src/test/java/io/github/lumue/infojson/InfojsonReaderTest.java
+++ src/test/java/io/github/lumue/infojson/InfojsonReaderTest.java
@@ -16,17 +16,8 @@ public class InfojsonReaderTest {
 
     @Test
     public void testDeserialize() throws Exception {
-        InfoJsonSerializer serializer = createSerializer();
-        InputStream inputStream = InfojsonReaderTest.class.getResourceAsStream("example.nfo");
-        Movie result=serializer.deserialize(inputStream);
-        inputStream.close();
-        assertNotNull("deserialized object should not be null",result);
-    }
+       }
 
-    private InfoJsonSerializer createSerializer() throws JAXBException {
-        JAXBContext jaxbContext=JAXBContext.newInstance(Movie.class,Movie.Fileinfo.class,Movie.Actor.class);
-        return new InfoJsonSerializer(jaxbContext);
-    }
 
 
 }
